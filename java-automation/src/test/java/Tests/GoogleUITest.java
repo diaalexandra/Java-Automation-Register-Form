58b76de2db90c7cceb570eab8e4d62d3dffb0f1a
diaalexandra <stanicoaeaalexandra@gmail.com>
2023-06-08 23:28:55 +0300

    Java-Automation-Register-Form

---
+++ java-automation/src/test/java/Tests/GoogleUITest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GoogleUITest {
+    WebDriver driver;
+    String url;
+    String browser;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Test
+    public void SearchOnGoogleTest() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+        WebElement acceptCookieButton = driver.findElement(By.id("L2AGLb"));
+        acceptCookieButton.click();
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys("Google Atelierul Digital");
+        Thread.sleep(100);
+        searchInput.sendKeys(Keys.ENTER);
+    }
+
+
+}
---
+++ java-automation/src/test/java/dataProviders/SignUpPageDataProvider.java
@@ -0,0 +1,14 @@
+package dataProviders;
+
+import org.testng.annotations.DataProvider;
+
+public class SignUpPageDataProvider {
+    @DataProvider(name = "signUpPageDataProvider")
+    public Object[][] SignUpPageDataProvider() {
+        return new Object[][] {
+                {"chrome", true},
+//                {"firefox", true},
+//                {"edge", true},
+        };
+    }
+}
